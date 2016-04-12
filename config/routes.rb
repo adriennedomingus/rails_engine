73c49bf6342567e57e0f6b7772011686e8e8a281
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-11 19:54:52 -0600

    Implement Merchant relationships
    New controller for items and invoices

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
       get "/merchants/find", to: "merchants/finder#show"
       get "/merchants/find_all", to: "merchants/finder#index"
       get "/merchants/random", to: "merchants/random#show"
+      get "/merchants/:id/items", to: "merchants/items#index"
+      get "/merchants/:id/invoices", to: "merchants/invoices#index"
 
       get "/transactions/find", to: "transactions/finder#show"
       get "/transactions/find_all", to: "transactions/finder#index"
--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -32,4 +32,31 @@ RSpec.describe "Merchant endpoint" do
     get "/api/v1/merchants/random.json"
     expect(response.status).to eq(200)
   end
+
+  it "returns items associted with a merchant" do
+    m1 = Merchant.create(name: "merchant 1")
+    m2 = Merchant.create(name: "merchant 2")
+    m1.items.create(name: "name", description: "description", unit_price: 10.88)
+    m1.items.create(name: "other name", description: "other description", unit_price: 12.88)
+    m2.items.create(name: "another name", description: "another description", unit_price: 14.88)
+
+    get "/api/v1/merchants/#{m1.id}/items"
+    result = JSON.parse(response.body)
+
+    expect(result.count).to eq(2)
+  end
+
+  it "returns invoices associated with a merchant" do
+    customer = Customer.create(first_name: "adrienne", last_name: "domingus")
+    customer2 = Customer.create(first_name: "other name", last_name: "last name")
+    merchant = Merchant.create(name: "the merchant")
+    merchant2 = Merchant.create(name: "the merchant")
+    customer.invoices.create(merchant_id: merchant.id, status: "shipped")
+    customer2.invoices.create(merchant_id: merchant2.id, status: "shipped")
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+    result = JSON.parse(response.body)
+
+    expect(result.count).to eq(1)
+  end
 end
